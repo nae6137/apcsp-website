89147086dd4ed3ce018e02b875ae14dde4d2bcfa
nae6137 <emilyna99@gmail.com>
2018-06-05 19:59:13 +0000

    first commit

---
+++ basiccalc.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+
+int main()
+
+{
+    float a;
+    float b;
+    float remainingmoney;
+    printf("\nEnter the value of a\n");
+    scanf ("%f", &a);
+    printf("\nEnter the value of b\n");
+    scanf ("%f", &b);
+
+    remainingmoney = a + b;
+    printf("Remaining Money: %f\n",remainingmoney );
+    return 0;
+}
+
+
+
