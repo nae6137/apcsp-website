563fe01a9cf6d41ba4f41de7fa954bccd9c24b46
nae6137 <emilyna99@gmail.com>
2018-06-07 20:20:45 +0000

    part

---
+++ add.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main()
+
+
+{
+
+    float budget;
+    float expense;
+    float remainingmoney;
+    printf("\nEnter the value of budget\n");
+    scanf ("%f", &budget);
+    printf("\nEnter the value of expense\n");
+    scanf ("%f", &expense);
+
+    remainingmoney = budget - expense;
+    printf("Remaining Money: %f\n",remainingmoney );
+    return 0;
+
+
+}
+
