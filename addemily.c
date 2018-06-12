1bdc4562d8b3d4fef9d2de802a3db8b94d17a5a1
nae6137 <emilyna99@gmail.com>
2018-06-12 19:38:18 +0000

    if statement works

--- addemily.c
+++ addemily.c
@@ -15,16 +15,17 @@ int main()
     scanf ("%f", &arg2expense);
 
     arg3remainingmoney = arg1budget - arg2expense;
+
+    if (arg3remainingmoney < 0){
+      printf("Out of money\n");
+    }
+      else{
+        printf("Still within budget!");
+    }
+
     printf("Remaining Money: %f\n",arg3remainingmoney );
-    if (arg3remainingmoney < 0);
-    {printf("Out of money\n");
-    }
-    else
-    {printf("Still within budget!")
-    }
+
 
     return 0;
-
-
 }
 
