332cc233946ee0007e1314832375cd5d016880a7
Emily Na <38960659+nae6137@users.noreply.github.com>
2018-06-13 05:25:58 -0700

    Update addemily.c

--- addemily.c
+++ addemily.c
@@ -14,16 +14,21 @@ int main()
     printf("\nEnter the value of expense\n");
     scanf ("%f", &arg2expense);
 
-    arg3remainingmoney = arg1budget - arg2expense;
+    arg3rema iningmoney = arg1budget - arg2expense;
 
+    {
+	  for (arg1budget = &arg1budget; arg2expense = &arg2budget);
+		{
+		printf("Out of your budget: %f, you spent %f, and your remaining money is: %f\n",arg3remainingmoney );
+		}
+	}
+      
     if (arg3remainingmoney < 0){
       printf("Out of money\n");
     }
       else{
         printf("Still within budget!");
     }
-
-    printf("Remaining Money: %f\n",arg3remainingmoney );
 
 
     return 0;
